b916b7846b1a84f2856ab163bee1f656e6d40408
lmy <my_li5@163.com>
2024-09-13 23:21:44 +0800

    开发通用布局，导航栏，底部栏等

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Button } from "antd";
+import { ProForm, ProFormText } from "@ant-design/pro-components";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Button type="primary">Primary Button</Button>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
---
+++ src/layouts/basicLayout/index.tsx
@@ -0,0 +1,129 @@
+"use client";
+import { GithubFilled, LogoutOutlined, UserOutlined } from "@ant-design/icons";
+import { ProConfigProvider, ProLayout } from "@ant-design/pro-components";
+import { ConfigProvider, Dropdown } from "antd";
+import React, { useState } from "react";
+import Image from "next/image";
+import SearchInput from "@/layouts/basicLayout/components/SearchInput";
+import Link from "next/link";
+import GlobalFooter from "@/components/GlobalFooter";
+import "./index.css";
+import {menus} from "../../../config/menu";
+import {useRouter} from "next/navigation";
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function BasicLayout({ children }: Props) {
+  const router = useRouter();
+  const [pathname, setPathname] = useState("/list/sub-page/sub-sub-page1");
+  const [num, setNum] = useState(40);
+  return (
+    <div
+      id="test-pro-layout"
+      style={{
+        height: "100vh",
+        overflow: "auto",
+      }}
+    >
+      <ProConfigProvider hashed={false}>
+        <ConfigProvider
+          getTargetContainer={() => {
+            return document.getElementById("test-pro-layout") || document.body;
+          }}
+        >
+          <ProLayout
+            layout={"top"}
+            title={"面试刷题网站"}
+            logo={
+              <Image
+                src="/assets/logo.png"
+                width={40}
+                height={40}
+                alt={"面试刷题网站"}
+              ></Image>
+            }
+            prefixCls="my-prefix"
+            location={{
+              pathname,
+            }}
+            siderMenuType="group"
+            avatarProps={{
+              title: "乐未殇",
+              src: "https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg",
+              size: "small",
+              render: (props, dom) => {
+                return (
+                  <Dropdown
+                    menu={{
+                      items: [
+                        {
+                          key: "userCenter",
+                          icon: <UserOutlined />,
+                          label: "个人中心",
+                        },
+                        {
+                          key: "logout",
+                          icon: <LogoutOutlined />,
+                          label: "退出登录",
+                        },
+                      ],
+                      onClick: async (event: { key: React.Key }) => {
+                        const { key } = event;
+                        if (key === "logout") {
+                          userLogout();
+                        } else if (key === "userCenter") {
+                          router.push("/user/center");
+                        }
+                      },
+                    }}
+                  >
+                    {dom}
+                  </Dropdown>
+                );
+              },
+            }}
+            actionsRender={(props) => {
+              if (props.isMobile) return [];
+              return [
+                <SearchInput key="search" />,
+                <a
+                  key="github"
+                  href="https://github.com/leweishang0/mianshi-next-frontend"
+                  target="_blank"
+                >
+                  <GithubFilled key="GithubFilled" />
+                </a>,
+              ];
+            }}
+            headerTitleRender={(logo, title, _) => {
+              return (
+                <a>
+                  {logo}
+                  {title}
+                </a>
+              );
+            }}
+            onMenuHeaderClick={(e) => console.log(e)}
+            // 定义有哪些菜单
+            menuDataRender={() => {
+              return menus
+            }}
+            // 定义了菜单项如何渲染
+            menuItemRender={(item, dom) => (
+              <Link href={item.path || "/"} target={item.target}>
+                {dom}
+              </Link>
+            )}
+              // 渲染底部栏
+            footerRender={() => {
+              return <GlobalFooter />;
+            }}
+          >
+            {children}
+          </ProLayout>
+        </ConfigProvider>
+      </ProConfigProvider>
+    </div>
+  );
+}
